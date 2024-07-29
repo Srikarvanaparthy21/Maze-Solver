6f30f55dc3b718ec11f37e7c4c76b7819465f915
srikar <srikarvanaparthy@gmail.com>
2024-07-29 23:03:10 +0530

    Your commit message describing the changes

--- Mazesolver.java
+++ Mazesolver.java
@@ -1,64 +1,64 @@
 import java.util.*;
 
-class Rat
-{
-    private static void solve(int i,int j,int a[][],int n,ArrayList<String> ans,String move,int vis[][],int di[],int dj[]){
-        if(i==n-1 && j==n-1){
-            ans.add(move);
-            return;
-        }
-        String dir="DLRU";
-
-        for(int idx=0;idx<4;i++){
-            int nexti=i+di[idx];
-            int nextj=j+dj[idx];
-            if(nexti>=0 && nextj>=0 && nexti < n && nextj < n && vis[nexti][nextj]==0 && a[nexti][nextj]==1){
-                vis[i][j]=1;
-                solve(nexti,nextj,a,n,ans,move+dir.charAt(idx),vis,di,dj);
-                vis[i][j]=0;
-            }
-        }
+class Solution {
+  private static void solve(int i, int j, int a[][], int n, ArrayList < String > ans, String move,
+    int vis[][]) {
+    if (i == n - 1 && j == n - 1) {
+      ans.add(move);
+      return;
     }
 
-
-    public static ArrayList<String> findpath(int m[][],int n){
-        int vis[][]=new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                vis[i][j] = 0;
-            }
-        }
-        ArrayList<String> ans= new ArrayList<>();
-        int di[]={+1,0,0,-1};
-        int dj[]={0,-1,+1,0};
-
-        if(m[0][0]==1) solve(0,0,m,n,ans,"",vis,di,dj);
-        return ans;
+    if (i + 1 < n && vis[i + 1][j] == 0 && a[i + 1][j] == 1) {
+      vis[i][j] = 1;
+      solve(i + 1, j, a, n, ans, move + 'D', vis);
+      vis[i][j] = 0;
     }
-}
 
-class Main{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] a = new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                a[i][j]=sc.nextInt();
-            }
-        }
-        Rat obj = new Rat();
-        ArrayList < String > res = obj.findpath(a, n);
-        if (res.size() > 0) {
-            for (int i = 0; i < res.size(); i++)
-            System.out.print(res.get(i) + " ");
-            System.out.println();
-        }
-        else {
-            System.out.println(-1);
-        }
+    if (j - 1 >= 0 && vis[i][j - 1] == 0 && a[i][j - 1] == 1) {
+      vis[i][j] = 1;
+      solve(i, j - 1, a, n, ans, move + 'L', vis);
+      vis[i][j] = 0;
+    }
+
+    if (j + 1 < n && vis[i][j + 1] == 0 && a[i][j + 1] == 1) {
+      vis[i][j] = 1;
+      solve(i, j + 1, a, n, ans, move + 'R', vis);
+      vis[i][j] = 0;
+    }
+
+    if (i - 1 >= 0 && vis[i - 1][j] == 0 && a[i - 1][j] == 1) {
+      vis[i][j] = 1;
+      solve(i - 1, j, a, n, ans, move + 'U', vis);
+      vis[i][j] = 0;
+    }
+  }
+  public static ArrayList < String > findPath(int[][] m, int n) {
+    int vis[][] = new int[n][n];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        vis[i][j] = 0;
+      }
+    }
+
+    ArrayList < String > ans = new ArrayList < > ();
+    if (m[0][0] == 1) solve(0, 0, m, n, ans, "", vis);
+    return ans;
   }
 }
+class Main {
+  public static void main(String[] args) {
 
+    int n = 4;
+    int[][] a = {{1,0,0,0},{1,1,0,1},{1,1,0,0},{0,1,1,1}};
 
-
+    Solution obj = new Solution();
+    ArrayList < String > res = obj.findPath(a, n);
+    if (res.size() > 0) {
+      for (int i = 0; i < res.size(); i++)
+        System.out.print(res.get(i) + " ");
+      System.out.println();
+    } else {
+      System.out.println(-1);
+    }
+  }
+}
